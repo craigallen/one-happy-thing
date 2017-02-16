c04ccfd60b50a4d135b20ee635743df9b0122167
Craig Allen <craig@openwavesdesign.com>
2017-02-16 11:18:50 -0500

    Add new quotes

--- js/index.js
+++ js/index.js
@@ -1,17 +1,9 @@
 var quotes = [
-	'In order to succeed, we must first believe that we can. - Nikos Kazantzakis',
-	'If you can dream it, you can do it. - Walt Disney',
-	'It does not matter how slowly you go as long as you do not stop. - Confucius',
-	'Always do your best. What you plant now, you will harvest later. - Og Mandino',
-	'It always seems impossible until its done. - Nelson Mandela',
-	'Keep your eyes on the stars, and your feet on the ground. - Theodore Roosevelt',
-	'Don\'t watch the clock; do what it does. Keep going. - Sam Levenson',
-	'Good, better, best. Never let it rest. \'Til your good is better and your better is best. - St. Jerome',
-	'Failure will never overtake me if my determination to succeed is strong enough. - Og Mandino',
-	'Life is 10% what happens to you and 90% how you react to it. - Charles R. Swindoll',
-	'A creative man is motivated by the desire to achieve, not by the desire to beat others. - Ayn Rand',
-	'We should not give up and we should not allow the problem to defeat us. - A. P. J. Abdul Kalam',
-	'Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy. - Norman Vincent Peale'
+	'There is only one happiness in this life, to love and be loved. - George Sand',
+	'Love is when the other person\'s happiness is more important than your own. - H. Jackson Brown, Jr.',
+	'Friends show their love in times of trouble, not in happiness. - Euripides',
+	'Happiness is not something you postpone for the future; it is something you design for the present. - Jim Rohn',
+	'Happiness is not something ready made. It comes from your own actions. - Dalai Lama'
 ]
 
 	var savedNumber = 0;
